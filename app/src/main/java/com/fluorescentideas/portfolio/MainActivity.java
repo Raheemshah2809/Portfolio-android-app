c9a1f80c9ed1f146998eca586b27ca12613cc852
“Raheemshah2809” <Raheemshah2809@gmail.com>
2021-01-17 14:13:35 +0000

    Initial commit

---
+++ app/src/main/java/com/fluorescentideas/portfolio/MainActivity.java
@@ -0,0 +1,36 @@
+package com.fluorescentideas.portfolio;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.web);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new Callback());
+        web.loadUrl("https://raheemshahdev.engineer/index.html");
+
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+    }
+}
